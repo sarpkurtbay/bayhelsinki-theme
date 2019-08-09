b86ab143e3c1226cfad3f04a96fec3a57351daa8
Sarp Kurtbay <kurtbaysarp@gmail.com>
2019-08-10 00:44:49 +0300

    postcss has been set up

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,36 @@
 var gulp = require('gulp')
-var sass = require('gulp-sass')
-var CleanCSS = require('gulp-clean-css')
+// var sass = require('gulp-sass')
+
+var postCSS = require('gulp-postcss')
+var cleanCSS = require('gulp-clean-css')
 var sourcemaps = require('gulp-sourcemaps')
+var concat = require('gulp-concat')
+
 var browserSync = require('browser-sync').create()
 var imagemin = require('gulp-imagemin')
 // var ghpages = require('gh-pages')
 
-sass.compiler = require('node-sass');
+// sass.compiler = require('node-sass');
 
-gulp.task("sass", function() {
-    return gulp.src("src/scss/stylesheet.scss")
+gulp.task("css", function() {
+    return gulp.src([
+        "src/css/typography.css",
+        "src/css/stylesheet.css"
+    ])
         .pipe(sourcemaps.init())    // start creating the sourcemap for the changes otherwise minified css will be on one row
-        .pipe(sass())               // change the scss to css
+        .pipe(
+            postCSS ([
+                require("autoprefixer"),
+                require("postcss-preset-env")({
+                    stage: 1,
+                    browsers: ["IE 11", "last 2 versions"]
+                })
+            ])
+        ) 
+        // .pipe(sass())               // change the scss to css
+        .pipe(concat("stylesheet.css"))
         .pipe(                      // minify css to one intense line
-            CleanCSS({ 
+            cleanCSS({ 
                 compatibility: 'ie8' 
             })
         )
@@ -49,7 +66,7 @@ gulp.task("watch", function () {    // run in the background until stopped if th
     // // watch for any changes in the src/index.html, if there's a change, run the task called "html"
     // gulp.watch("src/*.index", ["html"]).on('change', browserSync.reload);
 
-    gulp.watch("src/scss/stylesheet.scss", ["sass"])   // watch for any changes in the src/css/app.css, if there's a change, run the task called "sass"
+    gulp.watch("src/css/stylesheet.css", ["css"])   // watch for any changes in the src/css/app.css, if there's a change, run the task called "sass"
     gulp.watch("src/fonts/*", ["fonts"])              // watch for any changes in the src/fonts, if there's a change, run the task called "fonts"
     gulp.watch("src/img/*", ["images"])               // watch for any changes in the src/img, if there's a change, run the task called "images"
 })
@@ -60,5 +77,5 @@ gulp.task("watch", function () {    // run in the background until stopped if th
 
 // run as the default when gulp has been started in the command line
 // start by running the tasks: "html", "sass" and "watch" (which runs until it's paused with ctrl + C )
-gulp.task('default', ["sass", "watch", "fonts", "images"]);
+gulp.task('default', ["css", "watch", "fonts", "images"]);
   
